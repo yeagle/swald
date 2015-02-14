e9f39854e404702ec4642bbe9316975d66f16a70
Helen Steingroever <helensteingroever@145.107.12.188.as.surfnet.utelisys.net>
2015-02-14 12:28:08 +0100

    added log density of dwald_gamma for the R and C code

--- src/dwald_gamma.c
+++ src/dwald_gamma.c
@@ -238,6 +238,130 @@ double dwald_gamma_d(double t, double alpha, double tau, double kappa)
     }
     return(d);
 }
+
+
+double dwald_gamma_d_log(double t, double alpha, double tau, double kappa)
+{
+    double d;
+    
+    if (alpha == 1 && tau == 1 && kappa == 1 )
+    {
+        d = (- 1. / (2.*t)) - log(2.) -2*log(t);
+    }
+    else if (tau == 1)
+    {
+        d = log(alpha) - ((2.*alpha*kappa-1.)/(2.*t*pow(kappa,2))) + 
+        log(1. + erf( (alpha*kappa-1.) / (kappa*sqrt(2.*t)) )) -
+        log(2.) - 2*log(t) - log(kappa);
+    }
+    else
+    {
+        double L1; 
+        double L2; 
+        double L3; 
+        double L4; 
+        double C1; 
+        double C2; 
+                                                   
+        L1 = LaguerreL(-(.5)*tau+.5, .5, (.5)*pow(alpha*kappa-1.,2)/(pow(kappa,2)*t));
+        
+        L2 = LaguerreL(-(.5)*tau+.5, 3./2., (.5)*pow(alpha*kappa-1.,2)/(pow(kappa,2)*t));
+        
+        L3 = LaguerreL(-(.5)*tau, .5, (.5)*pow(alpha*kappa-1.,2)/(pow(kappa,2)*t));
+        
+        L4 = LaguerreL(-(.5)*tau, 3./2., (.5)*pow(alpha*kappa-1.,2)/(pow(kappa,2)*t));
+        
+        C1 = sin((.5)*M_PI*tau)*r_gamma(-(.5)*tau+3./2.);
+        
+        C2 = sqrt(2.)*pow(alpha,3)*pow(kappa,3)*sqrt(t);
+        
+        d = log(-(1.0/16.0)*pow(2.,((.5)*tau+.5))*alpha*exp(-(.5)*pow(alpha,2)/t)*pow(kappa,(-tau-3.))*
+        pow(t,(-(.5)*tau-7.0/2.0))*M_PI*
+        
+        (
+         
+         C1*
+         L1*
+         C2 +
+         
+         C1*
+         L1*
+         sqrt(2.)*alpha*pow(kappa,3)*tau*pow(t,(3./2.)) -
+         
+         C1*
+         L2*
+         C2 -
+         
+         C1*
+         L1*
+         sqrt(2.)*alpha*pow(kappa,3)*pow(t,(3./2.)) -
+         
+         3.*C1*
+         L1*
+         sqrt(2.)*pow(alpha,2)*pow(kappa,2)*sqrt(t) -
+         
+         C1*
+         L1*
+         sqrt(2.)*pow(kappa,2)*tau*pow(t,(3./2.)) +
+         
+         3.*C1*
+         L2*
+         sqrt(2.)*pow(alpha,2)*pow(kappa,2)*sqrt(t)-
+         
+         2.*
+         L3*
+         cos((.5)*M_PI*tau)*r_gamma(-(.5)*tau+2.)*pow(alpha,2)*pow(kappa,3)*t +
+         
+         2.*cos((.5)*M_PI*tau)*r_gamma(-(.5)*tau+2.)*
+         L4*
+         pow(alpha,2)*pow(kappa,3)*t +
+         
+         C1*
+         L1*
+         sqrt(2.)*pow(kappa,2)*pow(t,(3./2.)) -
+         
+         2.*
+         L3*
+         cos((.5)*M_PI*tau)*r_gamma(-(.5)*tau+2.)*pow(kappa,3)*pow(t,2) +
+         
+         3.*C1*
+         L1*
+         sqrt(2.)*alpha*kappa*sqrt(t) -
+         
+         3.*C1*
+         L2*
+         sqrt(2.)*alpha*kappa*sqrt(t) +
+         
+         4.*
+         L3*
+         cos((.5)*M_PI*tau)*r_gamma(-(.5)*tau+2.)*alpha*pow(kappa,2)*t -
+         
+         4.*cos((.5)*M_PI*tau)*r_gamma(-(.5)*tau+2.)*
+         L4*
+         alpha*pow(kappa,2)*t -
+         
+         C1*
+         L1*
+         sqrt(2.)*sqrt(t) +
+         
+         C1*
+         L2*
+         sqrt(2.)*sqrt(t) -
+         
+         2.*
+         L3*
+         cos((.5)*M_PI*tau)*r_gamma(-(.5)*tau+2.)*kappa*t +
+         
+         2.*cos((.5)*M_PI*tau)*r_gamma(-(.5)*tau+2.)*
+         L4*
+         kappa*t
+         
+         ))-
+        
+        log(r_gamma(tau)) -log(C1) - log(cos((.5)*M_PI*tau)) - log(r_gamma(-(.5)*tau+2.));
+    }
+    return(d);
+}
      
 SEXP  dwald_gamma(SEXP t, SEXP alpha, SEXP tau, SEXP kappa) {
   double d;
