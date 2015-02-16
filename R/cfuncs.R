c619a44f112a0c3d19b3819d7b8b9d27723c6749
yeagle <dominik.wabersich@gmail.com>
2015-02-16 20:24:29 +0100

    added pswald function in C

--- R/cfuncs.R
+++ R/cfuncs.R
@@ -34,3 +34,10 @@ dswald <- function(t, alpha, nu, theta, give_log=FALSE){
   return(d)
 }
 
+pswald <- function(t, alpha, nu, theta, lower.tail=TRUE, log.p=FALSE){
+  d <- vector("double", length=length(t))
+  for (i in 1:length(t)) {
+    d[i] <- .Call(pswald_c, t[i], alpha, nu, theta, lower.tail, log.p)
+  }
+  return(d)
+}
---
+++ src/pswald.c
@@ -0,0 +1,33 @@
+// Shifted Wald (Inverse Gaussian) CDF
+
+#include <R.h>
+#include <Rinternals.h>
+#include <Rmath.h>
+
+double pswald_p(double t, double alpha, double nu, double theta, int lower_tail, int log_p)
+{
+    double p;
+    double x;
+
+    if(log_p)
+      x = exp(t);
+    else
+      x = t;
+
+    p = pnorm((nu*(x-theta)-alpha) / sqrt((x-theta)), 0,1,1,0) + 
+        exp(2*alpha*nu) * pnorm(-(nu*(x-theta)+alpha) / sqrt((x-theta)), 0,1,1,0);
+    
+    return (lower_tail ? p : 1-p);
+}
+
+SEXP  pswald(SEXP t, SEXP alpha, SEXP nu, SEXP theta, SEXP lower_tail, SEXP log_p) {
+  double p;
+  SEXP value;
+    
+  p = pswald_p(REAL(t)[0], REAL(alpha)[0], REAL(nu)[0], REAL(theta)[0], LOGICAL(lower_tail)[0], LOGICAL(log_p)[0]);
+    
+  PROTECT(value = allocVector(REALSXP, 1));
+  REAL(value)[0] = p;
+  UNPROTECT(1);
+  return value;
+}     
